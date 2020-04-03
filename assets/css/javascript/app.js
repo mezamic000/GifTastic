82f53ed890a9294e88d869f03c0b42465b64d804
Firecraker <mezamic000@gmail.com>
2020-04-03 10:06:41 -0500

    created topic buttons using javascript

--- assets/css/javascript/app.js
+++ assets/css/javascript/app.js
@@ -1,19 +1,40 @@
-$("#find-emotion").on("click", function(event) {
-	var queryURL =
-		"http://api.giphy.com/v1/gifs/search?api_key=bncTca2KYj131Dgdey0Tr3tI051nbv14&q=emotions&limit=10";
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).then(function(response) {
-		console.log(response);
+$(document).ready(function() {
+	var topics = ["Happy", "Sad", "Angry", "Bored", "Hungry", "Tired"];
 
-		for (var i = 0; i < response.data.length; i++) {
-			var emotionImage = $("<img>");
-			var imageUrl = response.data[i].images.fixed_height.url;
-			emotionImage.attr("src", imageUrl);
-			emotionImage.attr("alt", "emotion image");
-
-			$("#images").prepend(emotionImage);
+	function createButtons() {
+		for (var i = 0; i < topics.length; i++) {
+			var button = document.createElement("button");
+			var text = document.createTextNode(topics[i]);
+			button.appendChild(text);
+			var myDiv = document.getElementById("buttons");
+			myDiv.appendChild(button);
 		}
+	}
+	createButtons();
+
+	$("#find-emotion").on("click", function(event) {
+		// event.preventDefualt();
+		console.log("clicked");
+
+		var queryURL =
+			"https://api.giphy.com/v1/gifs/search?api_key=bncTca2KYj131Dgdey0Tr3tI051nbv14&q=emotions&limit=10";
+
+		console.log(queryURL);
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).then(function(response) {
+			console.log(response);
+
+			for (var i = 0; i < response.data.length; i++) {
+				var emotionImage = $("<img>");
+				var imageUrl = response.data[i].images.fixed_height.url;
+				emotionImage.attr("src", imageUrl);
+				emotionImage.attr("alt", "emotion image");
+
+				$("#images").prepend(emotionImage);
+			}
+		});
 	});
 });
